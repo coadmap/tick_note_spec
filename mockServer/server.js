276e64a9fb63fda0bde0881140bde3102e9d2fcb
nozomi-iida <iida19990106@gmail.com>
2021-05-21 14:45:13 +0900

    fix: pathを追加

--- mockServer/server.js
+++ mockServer/server.js
@@ -1,27 +1,32 @@
 const express = require("express");
-const { ApolloServer, addMockFunctionsToSchema } = require("apollo-server-express")
-const { loadSchemaSync } = require('@graphql-tools/load');
-const { GraphQLFileLoader } = require('@graphql-tools/graphql-file-loader');
+const {
+  ApolloServer,
+  addMockFunctionsToSchema,
+} = require("apollo-server-express");
+const { loadSchemaSync } = require("@graphql-tools/load");
+const { GraphQLFileLoader } = require("@graphql-tools/graphql-file-loader");
 const dayjs = require("dayjs");
 
 const app = express();
 
-const schema = loadSchemaSync('schema.graphql', { loaders: [new GraphQLFileLoader()] });
-addMockFunctionsToSchema({ schema })
+const schema = loadSchemaSync("schema.graphql", {
+  loaders: [new GraphQLFileLoader()],
+});
+addMockFunctionsToSchema({ schema });
 
 const mocks = {
   DateTime: () => {
-    return dayjs().format()
+    return dayjs().format();
   },
   Date: () => {
-    return dayjs().format("YYYY-MM-DD")
-  }
-}
+    return dayjs().format("YYYY-MM-DD");
+  },
+};
 
-const server = new ApolloServer({ schema, mocks })
+const server = new ApolloServer({ schema, mocks });
 
 server.applyMiddleware({ app });
 
-app.listen({ port: 4000 }, () => {
+app.listen({ port: 4000, path: "/graphql" }, () => {
   console.log("Server is ready");
 });
